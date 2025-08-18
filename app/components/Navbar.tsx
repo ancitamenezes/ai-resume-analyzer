03b1f1808fa7d04bb5b562aaf17ca7f6409e8b32
ancitamenezes <ancitamenezes28@gmail.com>
2025-08-18 20:38:45 +0530

    Final

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,7 @@
-
 import {Link} from "react-router";
-import type {JSX} from "react";
 
-const Navbar:() => JSX.Element=()=>{
-    return(
+const Navbar = () => {
+    return (
         <nav className="navbar">
             <Link to="/">
                 <p className="text-2xl font-bold text-gradient">RESUMIND</p>
@@ -14,4 +12,4 @@ const Navbar:() => JSX.Element=()=>{
         </nav>
     )
 }
-export default Navbar;
+export default Navbar
